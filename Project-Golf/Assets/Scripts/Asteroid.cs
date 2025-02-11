5bc5c1f0d800ab8a4b9450bb0ec0f5c81430d581
0SC4R24 <oscarviudez24@gmail.com>
2025-02-11 17:25:52 +0100

    Added Line Renderer to Asteroid

--- Project-Golf/Assets/Scripts/Asteroid.cs
+++ Project-Golf/Assets/Scripts/Asteroid.cs
@@ -18,10 +18,15 @@ public class Asteroid : MonoBehaviour
     private LineRenderer _lineRenderer;
     private List<Planet> _planets;
     
+    [SerializeField] private int numSteps = 1000;
+    private int _index = 1;
+    
     private void Start()
     {
-        _lineRenderer = GetComponent<LineRenderer>();
         _rigidbody = GetComponent<Rigidbody>();
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.positionCount = 1;
+        _lineRenderer.SetPosition(0, _rigidbody.position);
         mass = surfaceGravity * radius * radius / SimulationManager.Instance.GetGravitationalConstant();
         transform.localScale = Vector3.one * radius;
         _rigidbody.mass = mass;
@@ -49,6 +54,23 @@ public class Asteroid : MonoBehaviour
     public void UpdatePosition(float timeStep)
     {
         _rigidbody.MovePosition(_rigidbody.position + _velocity * timeStep);
+        _lineRenderer.positionCount++;
+        _lineRenderer.SetPosition(_index++, _rigidbody.position);
+    }
+
+    public void StartSimulation()
+    {
+        _velocity = initialVelocity;
+    }
+    
+    public void StopSimulation()
+    {
+        _velocity = Vector3.zero;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        SimulationManager.Instance.StopSimulation();
     }
 
     public Vector3 GetPosition()
