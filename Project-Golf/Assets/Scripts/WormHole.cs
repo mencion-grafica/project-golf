2779c1f421b48b6c7d2f9c915c48bdb06d5c17eb
OscarMaestre987 <oscarmaestregonzalez@gmail.com>
2025-02-11 17:30:12 +0100

    cambios script

--- Project-Golf/Assets/Scripts/WormHole.cs
+++ Project-Golf/Assets/Scripts/WormHole.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 public class WormHole : Planet
 {
     [SerializeField] private WormHole planet; // Wormhole destino
-    public bool tpCouldown = false; // Control de cooldown
-    [SerializeField] private float teleportOffset = 2f; // Distancia extra en la dirección del movimiento
+    [SerializeField] private int teleportOffset = 5; // Distancia extra en la dirección del movimiento
 
     private void OnTriggerEnter(Collider other)
     {
-        if (planet != null && planet != this && !tpCouldown)
+        if (planet != null && planet != this)
         {
             Debug.Log("mover");
 
@@ -17,26 +16,11 @@ public class WormHole : Planet
             if (rb != null)
             {
                 Vector3 moveDirection = rb.velocity.normalized; // Obtener la dirección del movimiento
-                Vector3 teleportPosition = planet.GetCenter().position + moveDirection * 5; // Nueva posición con offset
-                Debug.Log(moveDirection);
-                Debug.Log(moveDirection * 5);
+                Vector3 teleportPosition = planet.GetCenter().position + moveDirection * teleportOffset; // Nueva posición con offset
                 other.transform.position = teleportPosition; // Teletransportar con desplazamiento
                 rb.velocity = rb.velocity; // Mantener la velocidad original
-
-                // Activar cooldown en ambos wormholes
-               // tpCouldown = true;
-               // planet.tpCouldown = true;
-
-                StartCoroutine(ResetCooldown());
-                planet.StartCoroutine(planet.ResetCooldown());
             }
         }
-    }
-
-    private IEnumerator ResetCooldown()
-    {
-        yield return new WaitForSeconds(0.1f); // Esperar 3 segundos
-        tpCouldown = false; // Desactivar cooldown
     }
 }
 
