380d0dd8704df63b45e2f439f468c5818c65d784
JuanluisSoto <117105701+JuanluisSoto@users.noreply.github.com>
2025-02-11 17:54:13 +0100

    Creacion ScenaChange

---
+++ Project-Golf/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void onClick() { 
+        SceneManager.LoadScene();
+    }
+
+
+}
