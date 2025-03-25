95ea26ba012e087e60192a55f720e0737365b993
JuanluisSoto <117105701+JuanluisSoto@users.noreply.github.com>
2025-03-25 16:33:50 +0100

    Fixed Rotation

--- Project-Golf/Assets/_Scripts/AsteroidRing.cs
+++ Project-Golf/Assets/_Scripts/AsteroidRing.cs
@@ -5,14 +5,14 @@ using UnityEngine.Serialization;
 
 public class AsteroidRing : MonoBehaviour
 {
-    [FormerlySerializedAs("rotationVelocity")]
+    [FormerlySerializedAs("rotationalVelocity")]
     [Tooltip("Valores inferiores a 1 haran que rote cada vez mas lento")]
     [SerializeField, Range(0, 50)]
-    private float rotationVelocity = 1.0f;
+    private float rotationalVelocity = 1.0f;
 
     void Update()
     {
-        Vector3 rotation = new Vector3 (0, rotationVelocity, 0);
+        Vector3 rotation = new Vector3 (0, rotationalVelocity, 0);
         transform.Rotate(rotation);
     }
     
