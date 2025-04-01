0aeb3e05f1bc88ca7702337e4eacad202324271f
0SC4R24 <oscarviudez24@gmail.com>
2025-04-01 16:04:00 +0200

    Revert "Merge branch 'main' into Map"
    This reverts commit f3c937b41c6a329f588de7bef2c2dbc23b1fd491, reversing
    changes made to 08a71455f5ceb9a7ff58faa5b855e2b831e3622a.

--- Project-Golf/Assets/_Scripts/Planet.cs
+++ Project-Golf/Assets/_Scripts/Planet.cs
@@ -9,13 +9,11 @@ public class Planet : MonoBehaviour
 {
     [SerializeField] private SOLevelData.PlanetType type;
     [SerializeField, Range(0.0f, 100000.0f)] private float mass = 1.0f;
-    private bool _isActive = false;
     
     private Transform _center;
     private Rigidbody _rigidbody;
     private XRGrabInteractable _grabInteractable;
     private Collider collider;
-    private int childCount = 0;
     
     private void Start()
     {
@@ -25,17 +23,8 @@ public class Planet : MonoBehaviour
         _grabInteractable = GetComponent<XRGrabInteractable>();
         //_grabInteractable.useDynamicAttach = false;
         collider = GetComponent<Collider>();
-        _isActive = false;
-        childCount = GetChildCount() + 1;
     }
 
-    private int GetChildCount()
-    {
-        int count = 0;
-        foreach (Transform child in transform) count++;
-        return count;
-    }
-    
     public void IsDynamicAttach()
     {
         IXRInteractor interactor = _grabInteractable.GetOldestInteractorSelecting();
@@ -48,36 +37,6 @@ public class Planet : MonoBehaviour
         {
             _grabInteractable.useDynamicAttach = true;
         }
-    }
-
-    private List<Vector3> GetPlanetPoints()
-    {
-        List<GameObject> planetPoints = new List<GameObject>(GameObject.FindGameObjectsWithTag("PlanetPoint"));
-        List<Vector3> pointsPos = new List<Vector3>();
-        foreach (GameObject planetPoint in planetPoints) pointsPos.Add(planetPoint.transform.position);
-        return pointsPos;
-    }
-    
-    private string GetAttachName(string name)
-    {
-        int start = name.IndexOf("[");
-        int end = name.IndexOf("]");
-        return name.Substring(start + 1, end - start - 1);
-    }
-    
-    public void SetActive(bool active)
-    {
-        _isActive = false;
-        if (!active) return;
-        string attachName = GetAttachName(transform.GetChild(childCount - 1).gameObject.name);
-        _isActive = attachName == "PlanetPoint";
-        Debug.Log(attachName);
-        Debug.Log("Planet Active: " + _isActive);
-    }
-    
-    public bool IsActive()
-    {
-        return _isActive;
     }
     
     public float GetRadius()
--- Project-Golf/Assets/_Scripts/Shoot.cs
+++ Project-Golf/Assets/_Scripts/Shoot.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Shoot : MonoBehaviour
@@ -11,18 +10,9 @@ public class Shoot : MonoBehaviour
 
     public void shoot()
     {
-        var objs = GameObject.FindObjectsOfType<Asteroid>();
 
         SimulationManager.Instance.StopSimulation();
-
-        foreach (Asteroid asteroid in objs)
-        {
-            SimulationManager.Instance.RemoveCelestialBody(asteroid.gameObject);
-            Destroy(asteroid.gameObject);
-        }
-
-        
-        var obj = Instantiate(asteroid, this.transform.position, Quaternion.identity);
+        var obj =Instantiate(asteroid, this.transform.position, Quaternion.identity);
         SimulationManager.Instance.AddCelestialBody(obj);
         SimulationManager.Instance.StartSimulation();
     }
