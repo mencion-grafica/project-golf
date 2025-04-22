ee897aa51e68d19f9fe507d53f7fc6f77961d86e
Ghiojo <ghiojo@hotmail.com>
2025-04-22 15:57:01 +0200

    Updated Level Stuff
    Updated Level Selection, as well as adjusted player height (*AGAIN*)

--- Project-Golf/Assets/_Scripts/Level/LevelBox.cs
+++ Project-Golf/Assets/_Scripts/Level/LevelBox.cs
@@ -9,8 +9,25 @@ public class LevelBox : MonoBehaviour
     [SerializeField]
     private XRSocketInteractor ownInteractor;
 
+    [SerializeField] private GameObject levelScreen;
+
     public void OnCardInserted()
     {
-        LevelManager.Instance.LoadLevel(ownInteractor.GetOldestInteractableSelected().transform.GetComponent<LevelCard>()?.GetLevelData());
+        List<SOLevelData> levelData = ownInteractor.GetOldestInteractableSelected().transform.GetComponent<LevelCard>()
+            .GetLevelData();
+        foreach(SOLevelData data in levelData)
+        {
+            GameObject levelButton = Instantiate(Resources.Load<GameObject>("LevelButton"), levelScreen.transform);
+            levelButton.GetComponent<LevelButton>().SetLevelData(data);
+            //levelButton.GetComponent<LevelButton>().SetLevelName(data.levelName);
+        }
+    }
+    
+    public void OnCardRemoved()
+    {
+        foreach (Transform child in levelScreen.transform)
+        {
+            Destroy(child.gameObject);
+        }
     }
 }
